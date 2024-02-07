14f5b9acc474dd2831e9bf5637a5343797272974
param <param772003@gmail.com>
2024-02-07 20:12:00 +0530

    setting up index route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const Nexmo = require("nexmo");
 const socketio = require("socket.io");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
@@ -16,7 +17,11 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("TEST");
+});
 
 //Start server
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
