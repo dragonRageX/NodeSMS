a21bade4313ad16a86079ec6df5d0d95eb56dfb4
param <param772003@gmail.com>
2024-02-08 22:29:29 +0530

    send response back to client with socket io

--- client/js/main.js
+++ client/js/main.js
@@ -5,20 +5,28 @@ let response = document.querySelector(".response");
 
 button.addEventListener("click", send, false);
 
+const socket = io();
+socket.on("smsStatus", (data) => {   //catch 'data' (response) sent from server-side when the event 'smsStatus' occurs
+    response.innerHTML = `<h5>Text message sent to ${data.number}</h5>`;
+    numberInput.value = "";
+    textInput.value = "";
+})
+
 function send()
 {
     if(!(numberInput.value) || !(textInput.value))   //validation
     {
         response.innerHTML = "Please enter a valid Phone Number and Text Message!";
     }
-    else if((numberInput.value.length > 0) && (numberInput.value.length <= 10))
-    {
-        response.innerHTML = "Please include a valid Country Code along with the Phone Number!"
-    }
     else
     {
         const number = numberInput.value.replace(/\D/g, '');   //regex to remove all non-numeric characters from the input
         const text = textInput.value;
+        if((number.length > 0) && (number.length <= 10))
+        {
+            response.innerHTML = "Please include a valid Country Code along with the Phone Number!";
+            return;
+        }
     
         fetch("/", {
             method: "POST",
