5d2d11e3d24bc2e62e67e1093c5a43de048ba6f8
param <param772003@gmail.com>
2024-02-08 18:40:58 +0530

    send sms via nexmo and client-side validation

--- app.js
+++ app.js
@@ -17,11 +17,38 @@ app.use(express.static(__dirname + '/client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//initialise Nexmo
+const nexmo = new Nexmo({
+    apiKey: `${process.env.API_KEY}`,
+    apiSecret: `${process.env.API_SECRET}`
+}, { debug: true });
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("TEST");
 });
 
+//Catch form submit
+app.post("/", (req, res) => {
+    const number = req.body.number;
+    const text = req.body.text;
+
+    nexmo.message.sendSms(
+        `${process.env.MY_NUMBER}`, number, text, { type: "unicode" }, 
+        (err, responseData) => {
+            if(err)
+            {
+                console.log(err);
+            }
+            else
+            {
+                console.dir(responseData);
+            }
+        }
+    )
+});
+
 //Start server
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
+//to try - vonage api instead of nexmo api and axios instead of fetch api
--- client/js/main.js
+++ client/js/main.js
@@ -7,18 +7,25 @@ button.addEventListener("click", send, false);
 
 function send()
 {
-    const number = numberInput.value.replace(/\D/g, '');   //regex to remove all non-numeric characters from the input
-    const text = textInput.value;
-
-    fetch("/", {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify({ number: number, text: text })
-    })
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((err) => console.log(err));
+    if(!(numberInput.value) || !(textInput.value))   //validation
+    {
+        response.innerHTML = "Please enter a valid Phone Number and Text Message!";
+    }
+    else
+    {
+        const number = numberInput.value.replace(/\D/g, '');   //regex to remove all non-numeric characters from the input
+        const text = textInput.value;
+    
+        fetch("/", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({ number: number, text: text })
+        })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => console.log(err));
+    }
 }
